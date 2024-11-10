50c9bdd95de09f4813f2d41d619a73fa71786572
Avinash Nair <avinair108@gmail.com>
2024-11-09 23:47:31 -0500

    done with chatbots

---
+++ main.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+personality_prompt = input("Please enter your desired bot type: ")
+user_prompt = input("Please enter your prompt: ")
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "system",
+            "content": personality_prompt,
+        },
+        {
+            "role": "user",
+            "content": user_prompt,
+        },
+    ],
+    temperature=0.7,
+    max_tokens=64,
+    top_p=1,
+)
+
+print(response.choices[0].message.content)
